10014b7ca459e7ccc31a12441cdfc6ff14136dba
Maxim Matura <metura97@gmail.com>
2021-11-08 01:41:41 +0200

    add SSR

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Filter from '../Components/Filter/Filter';
+import * as miista from './miista-export.json';
 
-export default function Home() {
+let OFFSET = 6
 
-  const [offset, setOffset] = useState(6);
+export default function Home({ content }) {
+  const [offset, setOffset] = useState(OFFSET);
   let [goods, setGoods] = useState([]);
   const [color, setColor] = useState('All');
   const [min, setMin] = useState("");
@@ -31,54 +32,40 @@ export default function Home() {
   goods = goods.slice(0, offset);
 
   const loadMore = () => {
-    setOffset(offset + 6);
+    setOffset(offset + OFFSET);
   }
 
   useEffect(() => {
-
-    axios.get("miista-export.json")
-      .then(res => {
-        let filteredProducts = res.data.data.allContentfulProductPage.edges;
-
-        if (color !== "All") {
-          filteredProducts = filteredProducts.filter(element => {
-            if (element.node.colorFamily && element.node.colorFamily[0].name === color) {
-              return element;
-            }
-          });
+    let filteredProducts = content;
+    if (color !== "All") {
+      filteredProducts = filteredProducts.filter(element => {
+        if (element.color === color) {
+          return element;
         }
-
-        if (category !== "All") {
-          filteredProducts = filteredProducts.filter(element => {
-            if (element.node.categoryTags && element.node.categoryTags.includes(category)) {
-              return element;
-            }
-          });
+      });
+    }
+    if (category !== "All") {
+      filteredProducts = filteredProducts.filter(element => {
+        if (element.tags && element.tags.includes(category)) {
+          return element;
         }
-
-        if (min !== "") {
-          filteredProducts = filteredProducts.filter(element => {
-            if (element.node.shopifyProductEu.variants.edges && 
-                element.node.shopifyProductEu.variants.edges[0].node.price > +min) {
-                  return element;
-            }
-          });
-
+      });
+    }
+    if (min !== "") {
+      filteredProducts = filteredProducts.filter(element => {
+        if (+element.price > +min) {
+          return element;
         }
-
-        if (max !== "") {
-          filteredProducts = filteredProducts.filter(element => {
-            if (element.node.shopifyProductEu.variants.edges && 
-                element.node.shopifyProductEu.variants.edges[0].node.price < +max) {
-                  return element;
-            }
-          });
+      });
+    }
+    if (max !== "") {
+      filteredProducts = filteredProducts.filter(element => {
+        if (+element.price < +max) {
+          return element;
         }
-
-        setGoods(filteredProducts);
-      }
-      );
-
+      });
+    }
+    setGoods(filteredProducts);
   }, [color, min, max, category]);
 
   return (
@@ -87,34 +74,26 @@ export default function Home() {
       <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         {goods.map((item, index) => {
           if (item) {
-
             return (
-
               <div key={index} className="rounded overflow-hidden shadow-lg">
-                <img className="w-full" src={item.node.thumbnailImage.file.url} alt="Item" />
+                <img className="w-full" src={item.jpg} alt="Item" />
                 <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">{item.node.name}</div>
-
+                  <div className="font-bold text-xl mb-2">{item.name}</div>
                 </div>
                 <div className="px-6 pt-4 pb-2">
-                  <GetCategories arr={item.node.categoryTags} />
-                  <GetPrices arr={item.node.shopifyProductEu.variants.edges} />
+                  <GetCategories arr={item.tags} />
+                  <GetPrices arr={item.prices} />
                 </div>
               </div>
             );
-
           }
-
         })}
       </div>
-
       <div className='flex items-center justify-center p-10'>
         <button onClick={loadMore} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Load More</button>
       </div>
-
     </>
   );
-
 }
 
 const GetCategories = (props) => {
@@ -136,4 +115,21 @@ const GetPrices = (props) => {
     });
   }
   return lst;
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      content: miista.data.allContentfulProductPage.edges.map(element => ({
+        name: element.node.name,
+        jpg: element.node.thumbnailImage.file.url,
+        color: element.node.colorFamily ? element.node.colorFamily[0].name : null,
+        tags: element.node.categoryTags,
+        prices: element.node.shopifyProductEu.variants.edges,
+        price: element.node.shopifyProductEu.variants.edges ?
+          element.node.shopifyProductEu.variants.edges[0].node.price :
+          null
+      }))
+    }
+  }
 }
